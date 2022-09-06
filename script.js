57b309bdd47f6b0d11f67d0207f71602c43103a2
karyseaweed <karyhdev@gmail.com>
2022-09-06 09:47:16 -0400

    UPDATE: changed syntax for one of the tests; prettified the files

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-const googleDatabse =  [
+const googleDatabse = [
   'cats.com',
   'souprecipes.com',
   'flowers.com',
   'animals.com',
   'catpictures.com',
-  'myfavouritecats.com'
-]
-
+  'myfavouritecats.com',
+];
 
 const googleSearch = (searchInput, db) => {
-  const matches = db.filter(website => {
+  const matches = db.filter((website) => {
     return website.includes(searchInput);
-  })
-  return matches.length > 3 ? matches.slice(0, 3) : matches
-}
+  });
+  return matches.length > 3 ? matches.slice(0, 3) : matches;
+};
 
 console.log(googleSearch('com', googleDatabse));
 
 module.exports = googleSearch;
-
--- script.test.js
+++ script.test.js
@@ -1,29 +1,23 @@
 const googleSearch = require('./script');
 
-dbMock = [
-  'dogs.com',
-  'cheesepuff.com',
-  'disney.com',
-  'dogpictures.com',
-] 
+dbMock = ['dogs.com', 'cheesepuff.com', 'disney.com', 'dogpictures.com'];
 
-
-it('silly test', () => {
+test('silly test', () => {
   expect('hello').toBe('hello');
 });
-it('is running', () => {
+test('is running', () => {
   expect(googleSearch('dog', dbMock)).toExist;
 });
 
-it('is is working', () => {
-  expect(googleSearch('dog', dbMock)).toEqual(['dogs.com', 'dogpictures.com'])
+test('is is working', () => {
+  expect(googleSearch('dog', dbMock)).toEqual(['dogs.com', 'dogpictures.com']);
 });
 
-it('works with undefined and null input', () => {
-  expect(googleSearch(undefined, dbMock)).toEqual([])
-  expect(googleSearch(null, dbMock)).toEqual([])
+test('works with undefined and null input', () => {
+  expect(googleSearch(undefined, dbMock)).toEqual([]);
+  expect(googleSearch(null, dbMock)).toEqual([]);
 });
 
-it('does not return more than 3 matches', () => {
-  expect(googleSearch('.com', dbMock).length).toEqual(3)
+test('does not return more than 3 matches', () => {
+  expect(googleSearch('.com', dbMock).length).toEqual(3);
 });
